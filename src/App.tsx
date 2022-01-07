4b432fbc63570fcc1b01fb0fe273390c7930a088
Alan Poveda <alsepoveda@gmail.com>
2022-01-06 22:29:09 -0300

    Create Header"

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { Header } from './components/Header'
 import { GlobalStyle } from  "./styles/global"
 
 const Title = styled.h1`
@@ -9,10 +10,11 @@ const Title = styled.h1`
 
 export function App() {
   return (
-    <div className="App">
+    <>
+      <Header />
       <Title>Opa</Title>
       <GlobalStyle />
-    </div>
+    </>
   );
 }
 

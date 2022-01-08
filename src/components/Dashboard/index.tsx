f40358c00a44979582494267cc5659454c5ad21c
Alan Poveda <alsepoveda@gmail.com>
2022-01-08 13:09:19 -0300

    Summary config, only css

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from "../Dashboard/styles";
+import { Summary } from "../Summary";
+
+export const Dashboard = () => {
+    return(
+        <>
+            <Container>
+                <Summary />
+            </Container>
+        </>
+    );
+}
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,34 @@
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import totalImg from '../../assets/total.svg'
+
+
+import { Container } from "./styles";
+
+export const Summary = () => {
+    return(
+        <Container>
+            <div>
+                <header>
+                    <p>Income</p>
+                    <img src={incomeImg} alt="Income" />
+                </header>
+                <strong>R$2000,00</strong>
+            </div>
+            <div>
+                <header>
+                    <p>Outcome</p>
+                    <img src={outcomeImg} alt="Outcome" />
+                </header>
+                <strong>- R$1000,00</strong>
+            </div>
+            <div className='background-high-light'>
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="Total" />           
+                </header>
+                <strong>R$1000,00</strong>
+            </div>
+        </Container>
+    );
+}

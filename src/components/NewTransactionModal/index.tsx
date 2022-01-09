a1998623ed3124b5eedfd7edb1e9d20ee9e50b23
Alan Poveda <alsepoveda@gmail.com>
2022-01-09 11:09:00 -0300

    Modal css config with three inputs, register button and close button whit svg img

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,17 +1,37 @@
-import Modal from 'react-modal';
+import Modal from "react-modal";
+import { Container } from "./styles";
+import closeButton from "../../assets/close.svg";
 
-interface NewTransactionModalProps{
+interface NewTransactionModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
 }
 
-export const NewTransactionModal = ({ isOpen, onRequestClose }: NewTransactionModalProps) => {
+export const NewTransactionModal = ({
+    isOpen,
+    onRequestClose,
+}: NewTransactionModalProps) => {
     return (
         <Modal
             isOpen={isOpen}
             onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
         >
-            <h2>Register transaction</h2>
+            <button
+                type="button"
+                onClick={onRequestClose}
+                className="react-modal-close"
+            >
+                <img src={closeButton} alt="Close Modal" />
+            </button>
+            <Container>
+                <h2>Register transaction</h2>
+                <input placeholder="Title" />
+                <input placeholder="Amount" type="number" />
+                <input placeholder="Category" />
+                <button type="submit">Insert</button>
+            </Container>
         </Modal>
     );
 };
